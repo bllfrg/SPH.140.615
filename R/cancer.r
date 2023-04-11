e2abc2cabec9a3ee94302ec49b50c67cc1194924
Ingo Ruczinski <ingo@jhu.edu>
2023-04-11 19:00:04 -0400

    Added cancer data set

---
+++ R/cancer.r
@@ -0,0 +1,18 @@
+#' Cancer and survival time data
+#'
+#' Patients with advanced cancers of the stomach, bronchus, colon, ovary or breast were treated with ascorbate. The purpose of the study was to determine if patient survival differed with respect to the organ affected by the cancer. 
+#'
+#' @docType data
+#'
+#' @usage cancer
+#'
+#' @format A data frame with 64 rows and 2 columns. The response is numeric, the cancer type is a factor.
+#'
+#' @keywords datasets
+#'
+#' @source \href{https://biostat.jhsph.edu/~iruczins/teaching/140.615/}{Class page}
+#'
+#' @examples
+#' str(cancer)
+#' summary(cancer)
+"cancer"
