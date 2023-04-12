5713cffc67e97802ec1ad07c578ef0e48a2e8c60
Ingo Ruczinski <ingo@jhu.edu>
2023-04-12 12:00:53 -0400

    Added Pearson data set

---
+++ R/pear.r
@@ -0,0 +1,18 @@
+#' Pearson's data of fathers' and daughters' heights
+#'
+#' The heights of fathers and their daughters, reported by Karl Pearson around 1903.
+#'
+#' @docType data
+#'
+#' @usage pear
+#'
+#' @format A data frame with 1,376 rows and 2 columns.
+#'
+#' @keywords datasets
+#'
+#' @source \href{https://biostat.jhsph.edu/~iruczins/teaching/140.615/}{Class page}
+#'
+#' @examples
+#' str(pear)
+#' summary(pear)
+"pear"
